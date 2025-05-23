29bfb77f3a16f7fcb3961cf05f14c38f7b69b10a
Carlos-Natan <carlosnatan728@gmail.com>
2025-05-23 09:46:26 -0300

    Update Atividade210525.py

--- Atividade210525.py
+++ Atividade210525.py
@@ -5,12 +5,12 @@ while True:
     p1 = input("Informe a posição do usuário: ")
     pilha.append(p1)
 
-    o = input("\n0. Sair\n1. Informar dados\n2. Mostrar dados\n3. Marcar ordem de chegada\nEscolha uma opção: ")
+    o = input("\n0. sair\n1. informar dados\n2. mostrar dados\n3. marcar ordem de chegada\nescolha uma opcao: ")
     
     if o == "1":
-        n = input("Nome: ")
-        c = input("Cidade: ")
-        t = input("Trabalho: ")
+        n = input("NOME: ")
+        c = input("CIDADE: ")
+        t = input("TRABALHO: ")
         
         usuario = {
             "NOME": n,
@@ -21,14 +21,14 @@ while True:
         fila.append(usuario)
         
     elif o == "2":
-        print("\nUSUÁRIOS CADASTRADOS: ")
+        print("\nUSUARIOS CADASTRADOS: ")
         for u in fila:
             print(u)
-        print("\nORDEM DE CHEGADA (PILHA): ")
+        print("\nORDEM DE CHEGADA EM PILHA: ")
         print(pilha)
         
     elif o == "0":
-        print("Saindo...")
+        print("SAINDO...")
         break
         
     else:
